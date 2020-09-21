c3444eb3b008a22cb9abc52758745839bd3e12b8
wxue24 <xuewilliam5@gmail.com>
2020-09-20 20:43:54 -0700

    Final version

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: "user",
+      body: "scream body",
+      createdAt: "2020-08-01T22:27:00.056Z",
+      likeCount: 5,
+      commentCount: 2,
+    },
+  ],
+};
